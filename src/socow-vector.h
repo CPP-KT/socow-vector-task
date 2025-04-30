187c25b0bb811596f7d5e6877279998852c4ea31
Murysin Maksim <murisin.max@gmail.com>
2025-04-30 18:19:37 +0300

    Add type constraints in solution template

--- src/socow-vector.h
+++ src/socow-vector.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <cstddef>
+#include <type_traits>
 
 namespace ct {
 
 template <typename T, std::size_t SMALL_SIZE>
-class SocowVector {};
+class SocowVector {
+  static_assert(std::is_copy_constructible_v<T>, "T must have a copy constructor");
+  static_assert(std::is_nothrow_move_constructible_v<T>, "T must have a non-throwing move constructor");
+  static_assert(std::is_copy_assignable_v<T>, "T must have a copy assignment operator");
+  static_assert(std::is_nothrow_move_assignable_v<T>, "T must have a non-throwing move assignment operator");
+  static_assert(std::is_nothrow_swappable_v<T>, "T must have a non-throwing swap");
+};
 
 } // namespace ct
